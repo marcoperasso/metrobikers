56da1742c9c1587e61592d08ca6b386973a13b0e
matteocanessa <>
2013-05-20 23:02:21 +0200

    Test

--- metrobikers/newEmptyPHP.php
+++ metrobikers/newEmptyPHP.php
@@ -16,7 +16,7 @@ class Pages extends CI_Controller {
             show_404();
         }
 
-        $data['title'] = ucfirst($page); // Capitalize the first letter
+        $data['title'] = ucfirst($page); 
 
         $this->load->view('templates/header', $data);
         $this->load->view('pages/' . $page, $data);
