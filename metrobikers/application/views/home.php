10429abe2f451a6a12de89d6577a288e39c38e0b
matteocanessa <matcanessa@gmail.com>
2013-06-07 00:57:02 +0200

    Commit di prova per modoficare il nome dell'autore

--- metrobikers/application/views/home.php
+++ metrobikers/application/views/home.php
@@ -8,6 +8,7 @@
     <body>
         <?php $this->load->view('templates/visualheader'); ?>
         <p>Hello Urban Mover!</p>
+        
         <a href="<?php echo \base_url()?>register/register"> Register</a>
         <?php $this->load->view('templates/footer'); ?>
     </body>
