b9f253bc8ade4a2355404ea2a0449db7be80b95f
Marco <marco.perasso@gmail.com>
2013-06-13 12:42:18 +0200

    htaccess bivalente produzione/sviluppo

--- metrobikers/application/config/privateconst.php
+++ metrobikers/application/config/privateconst.php
@@ -4,16 +4,16 @@
  * Qui dovrebbero esserci tutit i delta di configurazione fra ambiente di sviluppo e di produzione
 */
 
-define('BASE_URL',                                                             'http://localhost/metrobikers/');
+define('BASE_URL',              'http://localhost:8888/ecommuters/');
 
-define('DATABASE_USER',                                                         'webuser');
-define('DATABASE_PASSWORD',							'webuser');
-define('DATABASE_HOST',                                                         'localhost');
-define('DATABASE_NAME',                                                         'metrobikers');
+define('DATABASE_USER',         'webuser');
+define('DATABASE_PASSWORD',     'webuser');
+define('DATABASE_HOST',         'localhost');
+define('DATABASE_NAME',         'metrobikers');
 
 
-define('MAIL_HOST',                                                             'ssl://smtp.gmail.com');
-define('MAIL_USER',                                                             'mtbgroupscout@gmail.com');
-define('MAIL_PASSWORD',                                                         'montoggio');
-define('MAIL_PORT',                                                             465);
+define('MAIL_HOST',             'ssl://smtp.gmail.com');
+define('MAIL_USER',             'mtbgroupscout@gmail.com');
+define('MAIL_PASSWORD',         'montoggio');
+define('MAIL_PORT',             465);
 ?>
