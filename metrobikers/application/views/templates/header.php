28ef1feb1063088f6ede9764573757496823579c
marcoperasso <marco.perasso@gmail.com>
2013-06-20 10:38:51 +0200

    cambiata pasword

--- metrobikers/application/views/templates/header.php
+++ metrobikers/application/views/templates/header.php
@@ -3,3 +3,4 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
 
+
