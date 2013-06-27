7411c1223a0dd58bcce420b4905e45b45404b68f
marcoperasso <marco.perasso@gmail.com>
2013-06-27 13:27:05 +0200

    gestione mappe

--- metrobikers/application/controllers/mobile.php
+++ metrobikers/application/controllers/mobile.php
@@ -5,24 +5,12 @@ if (!defined('BASEPATH'))
 
 class Mobile extends MY_Controller {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     * 		http://example.com/index.php/welcome
-     * 	- or -  
-     * 		http://example.com/index.php/welcome/index
-     * 	- or -
-     * Since this controller is set as the default controller in 
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see http://codeigniter.com/user_guide/general/urls.html
-     */
+  
     public function version() {
-        echo 1;//versione
-        echo 1;//OK
+        
+        $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode(array('version' => 1)));
     }
 
 }
