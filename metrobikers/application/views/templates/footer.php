7ae5a39736d10f55e4e680435855edfe3cb1a07c
marcoperasso <marco.perasso@gmail.com>
2013-09-10 11:59:23 +0200

    cambiata dicitura

--- metrobikers/application/views/templates/footer.php
+++ metrobikers/application/views/templates/footer.php
@@ -1,2 +1,2 @@
-<div class="centercontent">ECOmmuters è un prodotto di liberi sviluppatori. Tutti i diritti non sono riservati.</div>
+<div class="centercontent">ECOmmuters è un prodotto di liberi sviluppatori. Tutti i diritti sono riservati.</div>
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
