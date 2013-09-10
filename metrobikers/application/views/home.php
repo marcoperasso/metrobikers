4bee5ad9c6f69cbe9e6513a2703f2e8aa5362d44
marcoperasso <marco.perasso@gmail.com>
2013-09-10 11:52:23 +0200

    cambiato tipo mappa

--- metrobikers/application/views/home.php
+++ metrobikers/application/views/home.php
@@ -3,44 +3,43 @@
     <head>
         <?php $this->load->view('templates/header'); ?>
         <title>Benvenuto in ECOmmuters</title>
+        <style>
+            p
+            {
+                padding:0px 20px 0px 20px;
+            }
+        </style>
     </head>
 
     <body>
         <?php $this->load->view('templates/visualheader'); ?>
-        <p><b>«</b>&nbsp;Sono le azioni che contano. I nostri pensieri, per quanto 
-            buoni possano essere, sono perle false fintanto che non vengono trasformati 
-            in azioni. Sii il cambiamento che vuoi vedere nel mondo.&nbsp;<b>»</b>&nbsp;
-            <small><a href="http://it.wikipedia.org/wiki/Mahatma_Gandhi">(Mahatma Gandhi)</a></small></p>
+        <p style ="padding:0px 20px 0px 20px; margin-bottom: 0px;"><i>«Sono le azioni che contano. I nostri pensieri, per quanto 
+                buoni possano essere, sono perle false fintanto che non vengono trasformati 
+                in azioni. Sii il cambiamento che vuoi vedere nel mondo.»</i></p>
+        <p style="text-align: right; padding-right: 50px; margin:0px;"><small><a target="gandhi" href="http://it.wikipedia.org/wiki/Mahatma_Gandhi">(Mahatma Gandhi)</a></small></p>
         <p>Spostarsi in auto in città spesso significa sopportare ore di coda, perdere 
             tempo alla ricerca di parcheggi, stress, costi di carburante, assicurazioni, 
-            manutenzione, inquinamento.
-        <p>Ridurre questi aspetti negativi contribuirebbe a migliorare la qualità della vita di ognuno di noi.</p>
-        <p>Utilizzare la bicicletta é una possibile soluzione: tuttavia molte 
-            città non sono ancora a misura di bicicletta e spesso ad utilizzare un 
-            mezzo a due ruote sono ancora pochi temerari.</p>
-        <p><b>ecommuters.com si prefigge di colmare questa lacuna</b>: registrati, trova persone che 
+            manutenzione, inquinamento; ridurre questi aspetti negativi contribuirebbe a migliorare la qualità della vita di 
+            ognuno di noi. La vita sedentaria che spesso conduciamo ci obbliga inoltre a diete forzate per mantenerci in buona salute e
+            costose sedute di palestra.</p>
+        <p>La bicicletta è una possibile risposta a tutti questi problemi: tuttavia molte 
+            città non sono ancora a misura di ciclista e spesso ad utilizzare un 
+            mezzo a due ruote sono solo pochi temerari.</p>
+        <p><b>Con ECOmmuters cerchiamo di colmare questa lacuna</b>: registra i tuoi itinerari utilizzando l'applicazione per smartphone, trova persone che 
             percorrono in bicicletta il tuo stesso tragitto casa-lavoro ed unisciti a loro.
-            Facendo massa critica saremo più rispettati dagli automobilisti, saremo più visibili 
+            Facendo massa critica saremo più rispettati dagli automobilisti, godremo di maggiore visibilità
             aumentando la sicurezza dello spostamento, potremo chiedere a gran voce alle amministrazioni 
             locali la creazione di percorsi ciclabili perchè proporremo i tragitti che percorriamo 
-            tutti assieme tutti i giorni.</p>
-        <p><b>Questa è la missione di ecommuters.com</b>: facilitare la ricerca di compagni di viaggio 
-            casa-lavoro al fine di coinvolgere più persone possibile, verso un futuro in cui in città 
+            assieme tutti i giorni.</p>
+        <p><b>Questa è la missione di ECOmmuters</b>: facilitare la ricerca di compagni di viaggio 
+            al fine di coinvolgere più persone possibile per dare loro visibilità come singoli e come categoria, 
+            verso un futuro in cui in città 
             saranno poche auto a transitare tra migliaia di biciclette. </p>
+        <p>Ti riconosci in questi valori? Bene, <a href="register" title="Diventa un ECOmmuter!">diventa anche tu un ECOmmuter</a>!</p>
         <a href="/asset/other/ECOmmutersMobile.apk" >Scarica la App per Android</a>
-        <p><em>Richiesta email per aggiornamenti</em></p>
-        <p>Se interessati lasciare mail qui sotto</p>
-        <div ng-controller="MailCollector">
-            <form ng-submit="collectMail()">
-                <input type="text" ng-model="emailAddress" placeholder="insert your email address">
-                <input class="button" type="submit" value="ok">
-            </form>
-            <span>{{greetings}}</span>
-        </div>
-        <br />
         <br />
         <?php
-        $data = isset($this->routes) ? array('routes'=> $this->routes) : array();
+        $data = isset($this->routes) ? array('routes' => $this->routes) : array();
         $this->load->view('map', $data);
         ?>
         <?php $this->load->view('templates/footer'); ?>
