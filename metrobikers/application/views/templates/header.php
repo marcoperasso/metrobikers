460de83ca13bb003261d164b30f4e530bcdefa93
Matteo Canessa <matcanessa@gmail.com>
2013-09-22 14:52:26 +0200

    Refactoring css

--- metrobikers/application/views/templates/header.php
+++ metrobikers/application/views/templates/header.php
@@ -1,7 +1,5 @@
 <link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/base/jquery-ui.css" type="text/css" rel="stylesheet"/>
-<link href="<?php echo base_url()?>asset/css/bootstrap.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo base_url()?>asset/css/bootstrap-theme.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo base_url()?>asset/css/common.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo base_url()?>asset/css/ecommuters.css" type="text/css" rel="stylesheet" />
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
 
