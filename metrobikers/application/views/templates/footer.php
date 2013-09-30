6d8f76ca7e09304d86bd851961115f4ee45561f7
Matteo Canessa <matcanessa@gmail.com>
2013-09-30 23:48:48 +0200

    Usato tag HTML5 footer

--- metrobikers/application/views/templates/footer.php
+++ metrobikers/application/views/templates/footer.php
@@ -1,7 +1,7 @@
-<div class="row footer">
+<footer class="row">
     <div class="col-md-12">
         <p class="text-center">
        ECOmmuters è un prodotto di liberi sviluppatori. Tutti i diritti sono riservati.
         </p>
     </div>
-</div>
+</footer>
