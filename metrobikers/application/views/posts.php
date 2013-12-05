22474b5e54c4309d811a40e96d2cd77ffaa1fb94
Marco <Marco@PERASSO-ASUS>
2013-12-05 22:06:25 +0100

    disabilitata modifica

--- metrobikers/application/views/posts.php
+++ metrobikers/application/views/posts.php
@@ -3,7 +3,7 @@
     <h5 class="postheader">
         <?php echo $post->name . ' ' . $post->surname; ?>
     </h5>
-    <div class ="postbody <?php if ($post->userid == $user->id) echo 'changeable gendercontent'?>"><?php echo html_escape($post->content) ?></div>
+    <div class ="postbody <?php if ($post->userid == $user->id) echo ''?>"><?php echo html_escape($post->content) ?></div>
     <div class ="postfooter">Pubblicato il <?php echo $post->time ?></div>
     <?php
 }
